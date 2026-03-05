c043eed37d8a1c3f922f08a1c5b3d42be8131cdb
itesh-singh <itesh5906@gmail.com>
2026-03-05 09:23:23 +0530

    chore(admin): register purchase order items admin configuration

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import PurchaseOrder, PurchaseOrderItem
+from inventory.models import StockItem, StockMovement
 
 
 class PurchaseOrderItemInline(admin.TabularInline):
@@ -11,6 +12,27 @@ class PurchaseOrderItemInline(admin.TabularInline):
 class PurchaseOrderAdmin(admin.ModelAdmin):
     inlines = [PurchaseOrderItemInline]
     list_display = ("id", "supplier", "status", "order_date", "expected_delivery")
+
+    def save(self, *args, **kwargs):
+        previous_status = None
+        if self.pk:
+            previous_status = PurchaseOrder.objects.get(pk=self.pk).status
+
+        super().save(*args, **kwargs)
+
+        # Only run when status changes to RECEIVED
+        if previous_status != self.RECEIVED and self.status == self.RECEIVED:
+            for item in self.items.all():
+                stock_item, _ = StockItem.objects.get_or_create(
+                    product=item.product,
+                    defaults={"reorder_level": 0, "reorder_qty": 0},
+                )
+                StockMovement.objects.create(
+                    stock_item=stock_item,
+                    movement_type=StockMovement.IN,
+                    quantity=item.quantity,
+                    reference=f"PO-{self.id} received",
+                )
 
 
 @admin.register(PurchaseOrderItem)
